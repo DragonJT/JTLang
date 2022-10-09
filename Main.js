cdebd17efe55053cd2114ffdb9f12a143cd3a188
DragonJT <31539225+DragonJT@users.noreply.github.com>
2022-10-09 19:49:47 +0100

    Add textarea so user can edit program

--- Main.js
+++ Main.js
@@ -4,7 +4,7 @@ f32 Six(){
     6;
 }
 
-f32 Sub(f32 a,f32 b){
+f32 Sub(f32 a, f32 b){
     a-b;
 }
 
@@ -18,4 +18,31 @@ f32 main(){
 } 
 `;
 
-CompileAndRun(code);
+var div= document.createElement('div');
+document.body.appendChild(div);
+var button = document.createElement('button');
+div.appendChild(button);
+button.innerHTML = 'Compile and run';
+button.onclick = ()=>{
+    CompileAndRun(textarea.value);
+};
+
+var textarea = document.createElement('textarea');
+textarea.rows = '20';
+textarea.cols = '100';
+textarea.value = code;
+textarea.addEventListener('keydown', (e)=>{
+    if (e.key == 'Tab') {
+        e.preventDefault();
+        var start = textarea.selectionStart;
+        var end = textarea.selectionEnd;
+    
+        textarea.value = textarea.value.substring(0, start) + "\t" + textarea.value.substring(end);
+    
+        textarea.selectionStart =
+        textarea.selectionEnd = start + 1;
+    }
+});
+document.body.appendChild(textarea);
+
+
